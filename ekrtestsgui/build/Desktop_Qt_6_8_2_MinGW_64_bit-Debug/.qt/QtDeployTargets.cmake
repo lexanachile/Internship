efd2c53ec81d36f1975d9ebdc560909efdd79c5a
Aleksey Yarosh <yarashsei@gmail.com>
2025-07-07 09:29:42 +0300

    Доработана экр с применением тестового класса для gui

---
+++ ekrtestsgui/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_kontr_rab_FILE D:/cute_projects/kontr_rab/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/kontr_rab.exe)
+set(__QT_DEPLOY_TARGET_kontr_rab_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_kontr_rab_RUNTIME_DLLS D:/cute/6.8.2/mingw_64/bin/Qt6Widgets.dll;D:/cute/6.8.2/mingw_64/bin/Qt6Gui.dll;D:/cute/6.8.2/mingw_64/bin/Qt6Core.dll)
